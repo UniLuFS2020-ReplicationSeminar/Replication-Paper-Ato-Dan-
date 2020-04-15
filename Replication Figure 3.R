2e5e5117a8a2eae82c1c422ea8f4bbdc2d0d2c8c
Dan Schläger <dan.schlaeger@gmx.de>
2020-04-15 15:30:09 +0200

    progress figure 3 (5)

--- Replication Figure 3.R
+++ Replication Figure 3.R
@@ -58,6 +58,50 @@ head(disjunct1,1)
 dim(disjunct1) 
 
 
+######Percentage of each discipline (relative to the number of articles *accounted for*), per discipline
+#First, aggregate by a period of 2 years
+colnames(disjunct1)
+disjunct1_agg <- aggregate(disjunct1[,c(4:29)], by = list(disjunct1$TYP, disjunct1$JOURNAL), FUN=sum) #aggregating the study programmes (and ACCOUNTED) by listing through two year period variable and Journal. Unclear why ACCOUNTED is included here
+
+
+dim(disjunct1_agg) # head(disjunct1_agg)
+disjunct_percent<-cbind(PERIOD=disjunct1_agg[,1], sweep(disjunct1_agg[,3:28],1,disjunct1_agg[,28],`/`)*100)
+
+dim(disjunct_percent)
+disjunct_percent<-aggregate(disjunct_percent[,c(2:27)], by=list(disjunct_percent$PERIOD), FUN=mean)
+
+######## Graphs
+#With economics (1950:2012)
+disjunct_percent1<-disjunct_percent[26:56,c(1:6,9,12,13)]
+m_disjunct_percent<-melt(disjunct_percent1, id="Group.1")
+colnames(m_disjunct_percent)<-c("PERIOD", "DISCIPLINE", "FREQUENCY")
+
+p<-ggplot(m_disjunct_percent, aes(PERIOD,  FREQUENCY))
+p +geom_bar(stat="identity") + facet_grid(DISCIPLINE~., scale='free_y') + theme(legend.position="none")  + ggtitle("Who do Economists Cite?\n Most cited disciplines cited in Five top journals (1900-2012)\n") + theme(axis.text.x = element_text(size = 6)) + theme(axis.text.x = element_text(angle = 60, hjust = 1)) + labs(x = "\nYear", y = "% of citations to this discipline\n")+  theme(plot.title=element_text(family="Arial", face="bold", size=14))  + theme(strip.text.y = element_text(size = 5))+ theme(panel.background = element_rect(fill='white', colour='grey'))
+
+#Without economics (1950:2012)
+colnames(disjunct_percent)
+disjunct_percent2<-disjunct_percent[26:56,c(1,3:6,9,11,12,13,16)]
+
+m_disjunct_percent<-melt(disjunct_percent2, id="Group.1")
+m_disjunct_percent$linetype<-as.character(m_disjunct_percent$variable)
+colnames(m_disjunct_percent)<-c("PERIOD", "DISCIPLINE", "FREQUENCY", "linetype")
+head(m_disjunct_percent)
+tail(m_disjunct_percent,200)
+m_disjunct_percent$linetype[m_disjunct_percent$linetype == "FINANCE"]<-"solid"
+m_disjunct_percent$linetype[m_disjunct_percent$linetype == "MATHEMATICS"]<-"dotdash"
+m_disjunct_percent$linetype[m_disjunct_percent$linetype == "STATISTICS"]<-"12345678"
+m_disjunct_percent$linetype[m_disjunct_percent$linetype == "SOCIOLOGY"]<-"dotted"
+m_disjunct_percent$linetype[m_disjunct_percent$linetype == "BUSINESS"]<-"longdash"
+m_disjunct_percent$linetype[m_disjunct_percent$linetype == "POLSCIENCE"]<-"F1"
+m_disjunct_percent$linetype[m_disjunct_percent$linetype == "LAW"]<-"1F"
+m_disjunct_percent$linetype[m_disjunct_percent$linetype == "PSYCHOLOGY"]<-"twodash"
+m_disjunct_percent$linetype[m_disjunct_percent$linetype == "HEALTH"]<-"dashed"
+
+q<-ggplot(m_disjunct_percent, aes(x=PERIOD,  y=FREQUENCY, group=DISCIPLINE))
+q + geom_smooth(aes(group=DISCIPLINE), se=F, span=.4, linetype=m_disjunct_percent$linetype, color="black") +geom_point(aes(shape=DISCIPLINE, size = 3), alpha=7/10)+ ggtitle("\n") + theme(axis.text.x = element_text(size = 6)) + theme(axis.text.x = element_text(angle = 60, hjust = 1)) + labs(x = "\nPeriod", y = "% of citations\n")+  theme(plot.title=element_text(family="Arial", face="bold", size=14)) + theme(panel.background = element_rect(fill='white', colour='grey')) + scale_shape_manual(values=c("F", "B", "S", "P", "L", "p", "M", "s", "H")) +theme(legend.position="none") 
+
+
 
 
 

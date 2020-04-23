c11eaeec37ca35c451221c37ebe90c59b15f89fe
Dan Schläger <dan.schlaeger@gmx.de>
2020-04-23 15:13:38 +0200

    preogressing replication figure 3

--- Replication/Replication Figure 3.R
+++ Replication/Replication Figure 3.R
@@ -1,10 +1,14 @@
 library(ggplot2)
 library(tidyverse)
+install.packages("rio")
+library("rio")
 
 ####Figure 3
 ##who do economists cite?
-
-disjunct<-read.csv("Base5j_Disj_Redux.csv")
+here::here("Data")
+list.files()
+disjunct <- rio::import("Data/Base5j_Disj_Redux.csv")
+view(disjunct)
 
 ##dimensions of the data set:
 dim(disjunct) 
@@ -12,7 +16,7 @@ table(disjunct$JOURNAL, disjunct$YEAR)
 
 ##plotting the year and the number of references in percentage:
 p <- ggplot(disjunct, aes(factor(YEAR), NUMB_REF))
-p + geom_boxplot(outlier.size = NA) + #aren't the outliers still present in the plot??
+p + geom_boxplot(outlier.size = NA) + #aren't the outliers still present in the plot?? Function for removing outliers isnt correct anymore 
   ggtitle("Number of references in 5 Top Economics Journals\nAER, QJE, JPE, RES, ECTRCA\n") + 
   theme(plot.title=element_text(face="bold", size=14))+ 
   labs(x = "\nYear", y = "Percentage\n")+ #why is the description in the quotation marks not equal to the description of the layers in the plot?
@@ -21,19 +25,19 @@ p + geom_boxplot(outlier.size = NA) + #aren't the outliers still present in the 
   theme_bw() + #white background
   stat_summary(fun.y="median", geom="point", shape=23, size=2, fill="brown4") #fun.y is an outdated function; shows the median trend in the plot
 
-
 ##creating a new variable: "two year period" 
 disjunct$TYP <- disjunct$YEAR
-sin_labs <- paste(seq(1900,2010, by=2), seq(1902,2012, by=2),sep="-") #labs is not a good choice for a new character name because it is also the name of the function "labels". Better: sin_labs. 
-disjunct$TYP <- cut(disjunct$TYP, breaks = seq(1900,2012, by = 2), labels = sin_labs, include.lowest = T) #why is there a seq interval of 2?
+TYP_labs <- paste(seq(1900,2010, by=2), seq(1902,2012, by=2),sep="-") #labs is not a good choice for a new character name because it is also the name of the function "labels". Better: TYP_labs. 
+disjunct$TYP <- cut(disjunct$TYP, breaks = seq(1900,2012, by = 2), labels = TYP_labs, include.lowest = T) #There is a seq intercal of two, because the plot in the paper visualizes two year period
 
 ##unaccounted citations of fields that are not relevant for figure 3 (DK + OTHERS)
 #here is not clear. what DK means
 colnames(disjunct)
-disjunct$UNACC<-apply(disjunct[,c(9,25)],1,sum) #why do they choose this values of c()?
+disjunct$UNACC<-apply(disjunct[,c(9,25)],1,sum) #they indicate all rows in a vector of the columns "OTHER" and "DK" because they will not be accounted
 disjunct$TOTAL_CITES<-apply(disjunct[,c(5:31)], 1, sum) 
-disjunct$ACCOUNTED<-disjunct$TOTAL_CITES-disjunct$UNACC #makes perfectly sense, but one could also calculate it in positive way
-disjunct$UNACC_RATE <- (disjunct$UNACC/disjunct$TOTAL_CITES)*100 
+disjunct$ACCOUNTED <- disjunct$TOTAL_CITES-disjunct$UNACC #makes perfect sense, but one could also calculate it in positive way
+disjunct$UNACC_RATE <- (disjunct$UNACC/disjunct$TOTAL_CITES)*100 #what exactly do we calculate and for what reason?
+view(disjunct)
 
 ##Appendix
 ##plotting the Unaccounted-rate
@@ -42,7 +46,7 @@ p <- ggplot(disjunct, aes(factor(YEAR), UNACC_RATE)) + stat_boxplot(outlier.size
 p + geom_boxplot(outlier.size = NA) + 
   theme(plot.title=element_text(face="bold", size=14)) + 
   labs(x = "\nYear", y = "Percentage\n") + 
-  scale_x_discrete(breaks = seq(1900,2012,10), labels = seq(1900,2010,10)) + #again (like in line 19)
+  scale_x_discrete(breaks = seq(1900,2012,10), labels = seq(1900,2010,10)) + #again like above
   theme_bw()  +  #white background
   stat_summary(fun.y="median", geom="point", shape=23, size=2, fill="darkgoldenrod")
 
@@ -62,9 +66,8 @@ dim(disjunct1)
 colnames(disjunct1)
 disjunct1_agg <- aggregate(disjunct1[,c(4:29)], by = list(disjunct1$TYP, disjunct1$JOURNAL), FUN=sum) #aggregating the study programmes (and ACCOUNTED) by listing through two year period variable and Journal. Unclear why ACCOUNTED is included here
 
-
 dim(disjunct1_agg) # head(disjunct1_agg)
-disjunct_percent<-cbind(PERIOD=disjunct1_agg[,1], sweep(disjunct1_agg[,3:28],1,disjunct1_agg[,28],`/`)*100)
+disjunct_percent<-cbind(PERIOD=disjunct1_agg[,1], sweep(disjunct1_agg[,3:28],1,disjunct1_agg[,28],`/`)*100) #why didnt they exclude ACCOUNTED in the previous definition?
 
 dim(disjunct_percent)
 disjunct_percent<-aggregate(disjunct_percent[,c(2:27)], by=list(disjunct_percent$PERIOD), FUN=mean)
@@ -100,5 +103,5 @@ m_disjunct_percent$linetype[m_disjunct_percent$linetype == "HEALTH"]<-"dashed"
 q<-ggplot(m_disjunct_percent, aes(x=PERIOD,  y=FREQUENCY, group=DISCIPLINE))
 q + geom_smooth(aes(group=DISCIPLINE), se=F, span=.4, linetype=m_disjunct_percent$linetype, color="black") +geom_point(aes(shape=DISCIPLINE, size = 3), alpha=7/10)+ ggtitle("\n") + theme(axis.text.x = element_text(size = 6)) + theme(axis.text.x = element_text(angle = 60, hjust = 1)) + labs(x = "\nPeriod", y = "% of citations\n")+  theme(plot.title=element_text(family="Arial", face="bold", size=14)) + theme(panel.background = element_rect(fill='white', colour='grey')) + scale_shape_manual(values=c("F", "B", "S", "P", "L", "p", "M", "s", "H")) +theme(legend.position="none") 
 
-#Hallo Hallo Comment 
+
 
